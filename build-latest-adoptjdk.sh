3880ad82741a6d563765fd00f2884b716ed812c6
eater <=@eater.me>
2019-05-18 17:16:33 +0200

    Fix versions and revisions

--- build-latest-adoptjdk.sh
+++ build-latest-adoptjdk.sh
@@ -1,7 +1,7 @@
 for jdk in openjdk8 openjdk10 openjdk11 openjdk12; do
   JSON="$(curl "https://api.adoptopenjdk.net/v2/latestAssets/releases/${jdk}?os=linux&arch=x64&release=latest&type=jdk")"
 
-  while read heap_size name link impl; do
+  while read heap_size name link impl semver time; do
     mod=""
     if [[ "${heap_size}" = "large" ]]; then
       mod="large-heapsize"
@@ -13,11 +13,13 @@ for jdk in openjdk8 openjdk10 openjdk11 openjdk12; do
 
     HINTS="""
 IMPLEMENTOR=AdoptOpenJdk
-SOURCE=$impl
-    """
+SOURCE=${impl}
+JAVA_VERSION=${semver}
+JAVA_VERSION_DATE=$(cut -c -10 <<< "${time}")
+"""
 
-    wget -nc -O "binaries/${name}" "$link";
+    wget -nc -O "binaries/${name}" "${link}";
     bash ./build-xbps.sh "$(realpath binaries/${name})" "${mod}" "${HINTS}";
     echo "${link}" >> known_binaries
-  done < <(jq -r 'map("\(.heap_size) \(.binary_name) \(.binary_link) \(.openjdk_impl)")[]' <<< "${JSON}")
+  done < <(jq -r 'map("\(.heap_size) \(.binary_name) \(.binary_link) \(.openjdk_impl) \(.version_data.semver) \(.timestamp)")[]' <<< "${JSON}")
 done
